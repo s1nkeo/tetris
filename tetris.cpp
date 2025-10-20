914a6238faa8f417905e891d07cdb01984aadb77
Sviatoslav Radchenko <svatikr@gmail.com>
2025-10-20 17:20:43 +0200

    render function

--- tetris.cpp
+++ tetris.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+void render() {
+    system("cls");
+    printf("⬛\n");
+}
--- tetris.h
+++ tetris.h
@@ -1,4 +1,6 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 
+void render(); // render of game in terminal
+
 #endif TETRIS_H
